a71bebb9988bdea78c3826996c0defa20271c27d
Kevin S. Clarke <ksclarke@gmail.com>
2013-08-18 23:43:31 -0400

    added style checks and reformatted mojos

--- src/main/java/info/freelibrary/maven/DeleteSolrCoreMojo.java
+++ src/main/java/info/freelibrary/maven/DeleteSolrCoreMojo.java
@@ -1,3 +1,4 @@
+
 package info.freelibrary.maven;
 
 import java.io.File;
@@ -31,64 +32,61 @@ import org.apache.maven.project.MavenProject;
 @Mojo(name = "delete-core")
 public class DeleteSolrCoreMojo extends AbstractMojo {
 
-	/**
-	 * The Maven project directory.
-	 */
-	@Component
-	private MavenProject myProject;
+    /**
+     * The Maven project directory.
+     */
+    @Component
+    private MavenProject myProject;
 
-	/**
-	 * The name of the new core to create.
-	 */
-	@Parameter(property = "solr.core.name", required = true)
-	private String myCore;
+    /**
+     * The name of the new core to create.
+     */
+    @Parameter(property = "solr.core.name", required = true)
+    private String myCore;
 
-	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		String targetDir = myProject.getBuild().getOutputDirectory();
-		File config = new File(targetDir, "solr.xml");
-		File coreDir = new File(targetDir, myCore);
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        String targetDir = myProject.getBuild().getOutputDirectory();
+        File config = new File(targetDir, "solr.xml");
+        File coreDir = new File(targetDir, myCore);
 
-		if (myCore.equals("collection1")) {
-			throw new MojoExecutionException(
-					"Sorry, but you can't delete the default core");
-		}
+        if (myCore.equals("collection1")) {
+            throw new MojoExecutionException(
+                    "Sorry, but you can't delete the default core");
+        }
 
-		try {
-			FileUtils.deleteDirectory(coreDir);
-		}
-		catch (IOException details) {
-			throw new MojoExecutionException(
-					"Unable to delete the requested core directory: "
-							+ coreDir.getAbsolutePath(), details);
-		}
+        try {
+            FileUtils.deleteDirectory(coreDir);
+        } catch (IOException details) {
+            throw new MojoExecutionException(
+                    "Unable to delete the requested core directory: " +
+                            coreDir.getAbsolutePath(), details);
+        }
 
-		if (config.exists()) {
-			try {
-				Document doc = new Builder().build(config);
-				Element root = doc.getRootElement();
-				Nodes nodes = root.query("//core[@name='" + myCore + "']");
+        if (config.exists()) {
+            try {
+                Document doc = new Builder().build(config);
+                Element root = doc.getRootElement();
+                Nodes nodes = root.query("//core[@name='" + myCore + "']");
 
-				for (int index = 0; index < nodes.size(); index++) {
-					nodes.get(index).detach();
-				}
+                for (int index = 0; index < nodes.size(); index++) {
+                    nodes.get(index).detach();
+                }
 
-				FileOutputStream xmlOut = new FileOutputStream(config);
-				Serializer serializer = new Serializer(xmlOut);
+                FileOutputStream xmlOut = new FileOutputStream(config);
+                Serializer serializer = new Serializer(xmlOut);
 
-				// Update the solr.xml configuration
-				serializer.setIndent(4);
-				serializer.write(doc);
-				xmlOut.close();
-			}
-			catch (ParsingException | IOException details) {
-				throw new MojoExecutionException(
-						"Can't write to the solr.xml file", details);
-			}
-		}
-		else if (getLog().isWarnEnabled()) {
-			getLog().warn("The solr.xml file isn't currently being used");
-		}
-	}
+                // Update the solr.xml configuration
+                serializer.setIndent(4);
+                serializer.write(doc);
+                xmlOut.close();
+            } catch (ParsingException | IOException details) {
+                throw new MojoExecutionException(
+                        "Can't write to the solr.xml file", details);
+            }
+        } else if (getLog().isWarnEnabled()) {
+            getLog().warn("The solr.xml file isn't currently being used");
+        }
+    }
 
 }
